9c3f8a07c0a70638d9baf2f32311b69dcf908e42
adrien leveille <adrienleveille.contact@gmail.com>
2024-05-02 17:40:56 +0200

    Adding play button

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,6 +8,7 @@ from game_stats import GameStats
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
+from button import Button
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -35,7 +36,10 @@ class AlienInvasion:
         self._create_fleet()
 
         # Start Alien invasion in an active state.
-        self.game_active = True
+        self.game_active = False
+
+        # Make the play button
+        self.play_button = Button(self, "Play")
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -47,7 +51,7 @@ class AlienInvasion:
                 self._update_bullets()
                 self._update_aliens()
                 self.ship.update()
-                
+
             self._update_screen()
             self.clock.tick(60)
 
@@ -185,6 +189,10 @@ class AlienInvasion:
             bullet.draw_bullet()
         self.ship.blitme()
         self.aliens.draw(self.screen)
+
+        # Draw the play button if the game is inactive.
+        if not self.game_active:
+            self.play_button.draw_button()
 
         pygame.display.flip()
 
