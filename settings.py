286ee676ec0928f999cb27dc52d9ae576c8c5eaa
adrien leveille <adrienleveille.contact@gmail.com>
2024-05-04 16:06:49 +0200

    Set up high score

--- settings.py
+++ settings.py
@@ -12,10 +12,11 @@ class Settings:
         self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_height = 15
-        self.bullet_width = 1000
-        self.bullet_color = (60, 60, 60)
+        self.bullet_height = 30
+        self.bullet_width = 4
+        self.bullet_color = (255, 70, 30)
         self.bullets_allowed = 3
+        self.bullet_speed = 20.0
 
         # Alien settings
         self.fleet_drop_speed = 10
@@ -30,7 +31,6 @@ class Settings:
     def initialize_dynamic_settings(self):
         """Initialize settings that change throughout the game."""
         self.ship_speed = 5.0
-        self.bullet_speed = 10.0
         self.alien_speed = 1.0
 
         # fleet_direction of 1 represents right; -1 represents left.
@@ -42,7 +42,7 @@ class Settings:
     def increase_speed(self):
         """Increase speed settings and aliens point values."""
         self.ship_speed *= self.speedup_scale
-        self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+        # self.bullet_speed *= self.speedup_scale
 
         self.alien_points = int(self.alien_points * self.score_scale)
